03f8bccc7e0ff89c07be1a5e42c012cc05c360da
romiagungn <romian1394@gmail.com>
2020-02-17 16:24:44 +0700

    challenge 14 database & ERD

---
+++ challenge14.sql
@@ -0,0 +1,22 @@
+CREATE TABLE jurusan(
+    id_jurusan varchar(50) primary key,
+    nama_jurusan varchar(50)
+);
+
+CREATE TABLE mahasiswa(
+    nim varchar(50) primary key,
+    nama varchar(50),
+    alamat varchar(50),
+    id_jurusan varchar(50)
+);
+
+CREATE TABLE mata_kuliah(
+    id_matkul varchar(50) primary key,
+    nama varchar(50),
+    sks varchar(50)
+);
+
+CREATE TABLE dosen(
+    id_dosen varchar(50) primary key,
+    nama varchar(50)
+);
